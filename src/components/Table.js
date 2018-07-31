355d6baf803a693f44d3a5acc0a25de62a391b7b
Serhii Filkovskyi <sfilkovskyi@oroinc.com>
2018-07-31 19:57:53 +0300

    rect-basics: -  rewrite from classComponent to functional component

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,32 +1,28 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Button  from './Button';
 
 function isSearched(pattern) {
     return function (item) {
         return item.title.toLocaleLowerCase().includes(pattern.toLowerCase());
     }
-};
+}
 
-class Table extends Component {
-    render() {
-        const {list, pattern, onDismiss} = this.props;
-
-        return (
-            <div className="table-wrapper">
-                {list.filter(isSearched(pattern)).map(item =>
-                    <div key={item.objectID}>
+const Table = ({list, pattern, onDismiss}) => {
+    return (
+        <div className="table-wrapper">
+            {list.filter(isSearched(pattern)).map(item =>
+                <div key={item.objectID}>
                         <span>
                             <a href={item.url}>{item.title}</a>
                         </span>
-                        <span>{item.author}</span>
-                        <span>{item.num_comments}</span>
-                        <span>{item.points}</span>
-                        <Button onClick={() => onDismiss(item.objectID)}>Remove</Button>
-                    </div>
-                )}
-            </div>
-        );
-    }
-}
+                    <span>{item.author}</span>
+                    <span>{item.num_comments}</span>
+                    <span>{item.points}</span>
+                    <Button onClick={() => onDismiss(item.objectID)}>Remove</Button>
+                </div>
+            )}
+        </div>
+    );
+};
 
 export default Table;
